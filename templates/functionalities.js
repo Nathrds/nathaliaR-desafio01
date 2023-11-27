7f8854642e7120121d42ecddf6b8f2f2522624da
Nathália <nathaliarsantos@live.com>
2023-11-27 17:09:54 -0300

    clean and organize the code

---
+++ templates/functionalities.js
@@ -0,0 +1,10 @@
+// Functionalities 
+function changeTextin() {
+    let button = document.getElementById("idButton");
+    button.innerHTML = "Just Go Ahead";
+}
+
+function changeTextout() {
+    let buttonOut = document.getElementById('idButton');
+    buttonOut.innerHTML = "Let's go";
+}
